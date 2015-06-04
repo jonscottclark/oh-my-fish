7617d402587e0a0dbba3078a73a3b774a7a82c08
Jon Scott Clark <jonscottclark@gmail.com>
2015-06-04 19:19:25 -0400

    Use different test for checking install warning suppression

--- functions/Plugin.fish
+++ functions/Plugin.fish
@@ -4,7 +4,7 @@ function Plugin --argument-names name
   if [ -e $fish_path/plugins/$name -o -e $fish_custom/plugins/$name ]
     import plugins/$name
   else
-    if ! $fish_suppress_install_warning
+    if not set -q fish_suppress_install_warning
       set_color yellow
       echo "Plugin '$name' is not installed. Run 'omf install' to download and install it."
       set_color normal
